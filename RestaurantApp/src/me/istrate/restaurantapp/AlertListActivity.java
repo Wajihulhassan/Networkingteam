a7017f27c93f2ad44deb431631467e9c63f10883
Wajih Ul Hassan <wajih.lums@gmail.com>
2014-04-23 19:10:41 -0400

    networking

---
+++ RestaurantApp/src/me/istrate/restaurantapp/AlertListActivity.java
@@ -0,0 +1,14 @@
+package me.istrate.restaurantapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlertListActivity extends Activity 
+{
+protected void onCreate(Bundle savedInstanceState) 
+{
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.alert_layout);
+}
+}
